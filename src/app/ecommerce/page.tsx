b08f355f76312c4de03c005e73f18735516c8733
Ahmed Haggag <a.haggagg@gmail.com>
2025-07-30 01:39:52 +0300

    Update page.tsx

--- src/app/ecommerce/page.tsx
+++ src/app/ecommerce/page.tsx
@@ -1,1 +1,12 @@
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 
+export default function PageName() {
+  return (
+    <>
+      <Breadcrumb pageName="Page Title" />
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+        {/* Page content goes here */}
+      </div>
+    </>
+  );
+}
